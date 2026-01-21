8af9eea8dd6b0d7cf8eeb3e20172d5e6110a11c5
ErikThroup <e.throupsanjuan@gmail.com>
2026-01-21 02:49:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import Experience from './src/Experience/Experience.js'
-import './style.css'
+
 
 const experience = new Experience(document.querySelector('canvas.webgl'))
 
 console.log(experience);	
+
 
