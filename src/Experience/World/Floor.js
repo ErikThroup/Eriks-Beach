42d6cdcb953724609d1798d1c15625274366432b
ErikThroup <e.throupsanjuan@gmail.com>
2026-03-29 20:37:46 +0200

    Refactor texture handling in Floor.js
    Removed normal texture from material setup and adjusted texture settings.

--- src/Experience/World/Floor.js
+++ src/Experience/World/Floor.js
@@ -19,28 +19,20 @@ export default class Floor {
     this.geometry = new THREE.CircleGeometry(50, 64)
   }
 
-  setTextures() {
+setTextures() {
     this.textures = {}
-
     this.textures.color = this.resources.items.grassColorTexture
     this.textures.color.encoding = THREE.sRGBEncoding
     this.textures.color.repeat.set(1.5, 1.5)
     this.textures.color.wrapS = THREE.RepeatWrapping
     this.textures.color.wrapT = THREE.RepeatWrapping
+}
 
-    this.textures.normal = this.resources.items.grassNormalTexture
-    this.textures.normal.repeat.set(1.5, 1.5)
-    this.textures.normal.wrapS = THREE.RepeatWrapping
-    this.textures.normal.wrapT = THREE.RepeatWrapping
-  }
-
-  setMaterial() {
+setMaterial() {
     this.material = new THREE.MeshStandardMaterial({
-      map: this.textures.color,
-      normalMap: this.textures.normal,
+        map: this.textures.color
     })
-  }
-
+}
   setMesh() {
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.mesh.rotation.x = -Math.PI * 0.5
