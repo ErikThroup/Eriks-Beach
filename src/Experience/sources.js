bdf1bade71a7fe7b4c02c685d6ca663751e03bfa
ErikThroup <e.throupsanjuan@gmail.com>
2026-03-29 20:38:35 +0200

    Remove wall textures from sources.js
    Removed wall color and normal textures from sources.

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -10,16 +10,6 @@ export default [
     path: "textures/dirt/normal.jpg",
   },
   {
-    name: "wallColorTexture",
-    type: "texture",
-    path: "textures/wall/color.jpg",
-  },
-  {
-    name: "wallNormalTexture",
-    type: "texture",
-    path: "textures/wall/normal.jpg",
-  },
-  {
     name: 'rockisland',      // You can rename this
     type: 'gltfModel',         // Must be 'gltfModel'
     path: 'models/rockisland1.glb'  // Change to your filename
