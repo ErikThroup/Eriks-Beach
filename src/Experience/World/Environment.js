c886205c6b255e07cb1011602b00798702f0f50d
ErikThroup <e.throupsanjuan@gmail.com>
2026-03-29 19:33:17 +0200

    Refactor Environment class and remove redundant code

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -4,13 +4,10 @@ import Physics from "../physics.js";
 
 export default class Environment {
   constructor() {
-    // Setup
     this.experience = new Experience()
     this.physics = new Physics();
     this.scene = this.experience.scene
     this.resources = this.experience.resources
-
-    // Debug
     this.debug = this.experience.debug
 
     if (this.debug.active) {
@@ -21,17 +18,6 @@ export default class Environment {
     this.setEnvironmentMap()
   }
 
-    update() {
-    // Update physics before everything else
-    if (this.physics) {
-      this.physics.update();
-    }
-    
-    this.camera.update();
-    this.world.update();
-    this.renderer.update();
-  }
-
   setSunLight() {
     this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
     this.sunLight.castShadow = true
@@ -40,7 +26,6 @@ export default class Environment {
     this.sunLight.shadow.normalBias = 0.05
     this.sunLight.position.set(3, 3, -2.25)
     this.scene.add(this.sunLight)
-
   }
 
   setEnvironmentMap() {
@@ -48,26 +33,21 @@ export default class Environment {
     this.environmentMap.intensity = 0.4
     this.environmentMap.texture = this.resources.items.environmentMapTexture
 
-    // this.environmentMap.texture.encoding = THREE.sRGBEncoding
-
     if (this.environmentMap.texture) {
       this.environmentMap.texture.encoding = THREE.sRGBEncoding
+      this.scene.environment = this.environmentMap.texture
     }
-
-    this.scene.environment = this.environmentMap.texture
 
     this.environmentMap.updateMaterials = () => {
       this.scene.traverse((child) => {
-        if (
-          child instanceof THREE.Mesh &&
-          child.material instanceof THREE.MeshStandardMaterial
-        ) {
+        if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
           child.material.envMap = this.environmentMap.texture
           child.material.envMapIntensity = this.environmentMap.intensity
           child.material.needsUpdate = true
         }
       })
     }
+
     this.environmentMap.updateMaterials()
   }
 }
