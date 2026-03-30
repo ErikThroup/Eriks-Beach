d1df1415890e417cac75c64b84f1355b9939ebd9
ErikThroup <e.throupsanjuan@gmail.com>
2026-03-31 01:21:57 +0200

    Add loading screen

--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -1,32 +1,27 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-
 import EventEmitter from './EventEmitter.js'
 
 export default class Resources extends EventEmitter {
   constructor(sources) {
     super()
 
-    // Options
     this.sources = sources
-    this.setLoaders()
-    this.startLoading()
-
-    // Setup
     this.items = {}
     this.toLoad = this.sources.length
     this.loaded = 0
+
+    this.setLoaders()
+    this.startLoading()
   }
 
   setLoaders() {
     this.loaders = {}
 
-    // Draco loader
     const dracoLoader = new DRACOLoader()
     dracoLoader.setDecoderPath('/Eriks-Beach/draco/')
 
-    // GLTF loader with Draco support
     this.loaders.gltfLoader = new GLTFLoader()
     this.loaders.gltfLoader.setDRACOLoader(dracoLoader)
 
@@ -56,7 +51,21 @@ export default class Resources extends EventEmitter {
     this.items[source.name] = file
     this.loaded++
 
+    // Update loading bar
+    const progress = (this.loaded / this.toLoad) * 100
+    const bar = document.getElementById('loading-bar')
+    if (bar) bar.style.width = `${progress}%`
+
     if (this.loaded === this.toLoad) {
+      // Hide loading screen after short delay
+      setTimeout(() => {
+        const screen = document.getElementById('loading-screen')
+        if (screen) {
+          screen.classList.add('hidden')
+          setTimeout(() => screen.remove(), 800)
+        }
+      }, 500)
+
       this.trigger('ready')
     }
   }
